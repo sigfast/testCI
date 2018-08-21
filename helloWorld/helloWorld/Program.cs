4cd088655af1d0eea7b6b00c5a1656b119667b79
Martin <martin.dollsen@solutionxperts.se>
2018-08-21 09:13:24 +0200

    init

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press any key");
+            Console.ReadKey(true);
+        }
+    }
+}
